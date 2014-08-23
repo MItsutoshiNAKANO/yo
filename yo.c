c45342dd2b98dfeaf67aa9c0518f01437e9da9d5
Mitsu <bkbin005@rinku.zaq.ne.jp>
2014-08-23 09:26:18 +0900

    First commit.
    	new file:   AUTHORS
    	new file:   COPYING
    	new file:   ChangeLog
    	new file:   INSTALL
    	modified:   LICENSE
    	new file:   Makefile.am
    	new file:   NEWS
    	new file:   autoscan.log
    	new file:   config.h.in
    	new file:   configure.ac
    	new file:   configure.scan
    	new file:   yo.c

--- yo.c
+++ yo.c
@@ -1,3 +1,4 @@
+/*
 The MIT License (MIT)
 
 Copyright (c) 2014 Mitsutoshi NAKANO <bkbin005@rinku.zaq.ne.jp>
@@ -19,3 +20,25 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+/**
+ * @file: yo.c
+ * Print yo. - sample program.
+ * @authoer: Mitsutoshi NAKANO <bkbin005@rinku.zaq.ne.jp>
+ * @since: 2014-07-28
+ */
+
+#include <stdio.h>
+
+/**
+ * Print yo.
+ * @return: Always 0.
+ * @since: 2014-07-28
+ */
+int
+main(void)
+{
+  printf("yo\n");
+  return 0;
+}
